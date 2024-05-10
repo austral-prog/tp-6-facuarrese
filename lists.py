e1d884ddf9d89175a5b835f3be849d527454f827
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-05-10 12:33:46 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,28 @@
-# Replace the "ANSWER HERE" with your answer
-
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
+def remove_element(list_to_remove_element):
+    length=len(list_to_remove_element)
+    print=length
+    if length>0:
+        del list_to_remove_element[0]
+    if length>4:
+        del list_to_remove_element[4]
+    if length>6:
+        del list_to_remove_element[5]
+    return (list_to_remove_element)
+def add_elements(lista):
+    lista.append('Yellow')
+    lista.insert(0,'pink')
+    return lista
+def is_empty(lista):
+    if len(lista) == 0:
+        print ("It is empty")
+    else:
+        print ("It is not empty")
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1)>3 and len(list_to_compare2)>3:
+        return list_to_compare1[2] == list_to_compare2[2]
+    else:
+        return 0==1
 
-
+    # Remove this line and implement
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return (list_of_lists_to_modify[0][0:2],list_of_lists_to_modify[1][1:len(list_of_lists_to_modify[1])-1],list_of_lists_to_modify[2][-2:])
